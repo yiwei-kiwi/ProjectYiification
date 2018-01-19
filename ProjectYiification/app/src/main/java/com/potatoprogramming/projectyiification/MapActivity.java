9e0277cf2c09ebe54dba95f2c3320870f48124e0
TheGentlemanCoder <santasmith123@hotmail.com>
2018-01-18 21:18:30 -0800

    Remove CMake library and functionality from project to decrease complexity.

--- ProjectYiification/app/src/main/java/com/potatoprogramming/projectyiification/MapActivity.java
+++ ProjectYiification/app/src/main/java/com/potatoprogramming/projectyiification/MapActivity.java
@@ -6,11 +6,6 @@ import android.widget.TextView;
 
 public class MapActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +13,7 @@ public class MapActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        tv.setText(R.string.app_name);
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
